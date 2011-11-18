decbb465ac3a7f617d201af9c6729339d995bce0
benny.lutati <benny.lutati@d1cde06e-e40a-6fc9-11d7-2fea08263baf>
2011-11-18 23:32:23 +0000

    some api fixes - still not so polished

--- AZAPI/src/bgu/csp/az/api/pgen/ProblemGenerator.java
+++ AZAPI/src/bgu/csp/az/api/pgen/ProblemGenerator.java
@@ -6,7 +6,6 @@ package bgu.csp.az.api.pgen;
 
 import bgu.csp.az.api.ProblemType;
 import bgu.csp.az.api.infra.Configureable;
-import java.util.Map;
 import java.util.Random;
 
 /**
@@ -16,7 +15,7 @@ import java.util.Random;
 public interface ProblemGenerator extends Configureable{
     public static final String CONFIGURATION_ID = "pgen";
     
-    void generate(Map<String, Object> variables, Problem p, Random rand);
+    void generate(Problem p, Random rand, float p1, float p2);
     String getName();
     ProblemType getType();
 }

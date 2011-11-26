80bca5716ee5742ec3dbe926225121f93052af69
benny.lutati <benny.lutati@d1cde06e-e40a-6fc9-11d7-2fea08263baf>
2011-11-26 14:35:08 +0000

    new ui - unattached

--- trunk/AZDevImpl/src/bgu/csp/az/dev/ui/RoundView.java
+++ trunk/AZDevImpl/src/bgu/csp/az/dev/ui/RoundView.java
@@ -14,10 +14,10 @@ package bgu.csp.az.dev.ui;
  *
  * @author bennyl
  */
-public class RoundView_ extends javax.swing.JPanel {
+public class RoundView extends javax.swing.JPanel {
 
     /** Creates new form RoundView */
-    public RoundView_() {
+    public RoundView() {
         initComponents();
         faildProblemPan.setVisible(false);
         debugProblemButtonPan.setVisible(false);

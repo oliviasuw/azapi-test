75545db47a72a120dbf2ada7d290a8943ce30904
benny.lutati <benny.lutati@d1cde06e-e40a-6fc9-11d7-2fea08263baf>
2011-11-29 11:21:10 +0000

    new ui - jobs left for the end of this version: debug screen - select a problem + save a problem problem view screen - questions

--- AZDevImpl/src/bc/tests/Test.java
+++ AZDevImpl/src/bc/tests/Test.java
@@ -7,44 +7,45 @@ package bc.tests;
 import bc.proxies.Operation;
 import bc.proxies.Wrapper;
 import bc.proxies.Wrapper.WrapConfigurator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
  * @author bennyl
  */
 public class Test {
-    
-    public static void main(String[] args){
-        IFC impl = new IMPL();
-        WrapConfigurator<IFC> conf = Wrapper.configure(IFC.class);
-        conf.perform(new Operation() {
 
-            @Override
-            public void operate(Object[] args) {
-                System.out.println("WRAPPED");
-            }
-        }).before().bla();
-        impl = conf.applyOn(impl);
+    public static void main(String[] args) {
+        Pattern compatabilityPattern1 = Pattern.compile("(\\s*((\\d+)\\s*=\\s*(\\d+))\\s*,)*\\s*((\\d+)\\s*=\\s*(\\d+))\\s*");
+        Pattern splitter1 = Pattern.compile(",*\\s*\\d+\\s*=\\s*\\d+");
+        Pattern seperator1 = Pattern.compile(",*\\s*(\\d+)\\s*=\\s*(\\d+)");
+
+        Pattern compatiblityPattern2 = Pattern.compile("\\s*(\\d+)\\s*vs\\.{0,1}\\s*(\\d+)\\s*");
         
-        impl.bla();
-    }
-    
-    public static interface IFC{
-        void work();
-        int bla();
-    }
-    
-    public static class IMPL implements IFC{
+        
 
-        @Override
-        public void work() {
-            System.out.println("REAL");
-        }
-
-        @Override
-        public int bla() {
-            throw new UnsupportedOperationException("Not supported yet.");
+        String x = "1   =2, 2 =   3,   4 = 5, 7 a= 9";
+        if (!compatabilityPattern1.matcher(x).matches()){
+            System.out.println("Bad!!!");
+            return;
         }
         
+        Matcher m = splitter1.matcher(x);
+        while (m.find()) {
+            String g0 = m.group();
+            Matcher mm = seperator1.matcher(g0);
+            mm.find();
+            System.out.println("xsi: '" + g0 + "' ");
+            System.out.println("= g1:'" + mm.group(1) + "', g2:'" + mm.group(2) + "'");
+
+        }
+        //        String[] xs = s.matcher(x)
+        //        for (String xsi : xs) {
+        //            Matcher m = sep.matcher(xsi);
+        //            System.out.println("xsi: '" + xsi + "' ");
+        //            System.out.println("= g1:'" + m.group(1) + "', g2:'" + m.group(2) + "'");
+        //        }
+
     }
 }

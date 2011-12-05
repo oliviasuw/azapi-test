ba5007978de96a09c24b8025c50a3d0a4b40547b
benny.lutati <benny.lutati@d1cde06e-e40a-6fc9-11d7-2fea08263baf>
2011-12-05 21:35:13 +0000

    problem view exposed - buggy

--- trunk/AZCoreImpl/src/bgu/dcr/az/impl/Registary.java
+++ trunk/AZCoreImpl/src/bgu/dcr/az/impl/Registary.java
@@ -8,8 +8,13 @@ import bgu.dcr.az.api.Agent;
 import bgu.dcr.az.api.ano.Algorithm;
 import bgu.dcr.az.api.ano.Register;
 import bgu.dcr.az.api.infra.Configureable;
+import bgu.dcr.az.impl.async.AsyncRound;
+import bgu.dcr.az.impl.pgen.ConnectedDCOPGen;
+import bgu.dcr.az.impl.stat.NCCCStatisticCollector;
+import bgu.dcr.az.impl.sync.SyncRound;
 import java.lang.reflect.Modifier;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import org.reflections.Reflections;
@@ -28,13 +33,24 @@ public enum Registary {
     Map<String, Class> agents = new HashMap<String, Class>();
 
     private Registary() {
-
         //Reflections ref = new Reflections("bgu.csp.az", new TypeAnnotationsScanner());
-
         Reflections ref = new Reflections(new ConfigurationBuilder().addUrls(ClasspathHelper.forPackage("bgu.dcr.az"), ClasspathHelper.forPackage("ext.sim")).setScanners(new TypeAnnotationsScanner()));
 
         //SCANNING XML ENTITIES
+        System.out.println("Scanning XML Entities");
         Set<Class<?>> types = ref.getTypesAnnotatedWith(Register.class);
+
+        if (types.size() == 0) {
+            //PROBABLY IN PROFILING SESSION WHEN THE CLASS PATH IS MISLEADING... 
+            System.out.println("reflections fail to scan class path - failback to hand registering (you probably running profiling seassion)");
+            types = new HashSet<Class<?>>();
+            types.add(SyncRound.class);
+            types.add(AsyncRound.class);
+            types.add(NCCCStatisticCollector.class);
+            types.add(ConnectedDCOPGen.class);
+            types.add(AlgorithmMetadata.class);
+        }
+
         for (Class<?> type : types) {
             if (type.isInterface() || type.isAnonymousClass() || Modifier.isAbstract(type.getModifiers())) {
                 System.out.println("Found Abstract Registered Item - ignoring it: " + type.getSimpleName());

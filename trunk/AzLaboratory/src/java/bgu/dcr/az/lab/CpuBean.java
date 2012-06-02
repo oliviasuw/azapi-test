8924bd60c28ab757a5839f9ce20104805249a9f8
inkago1@gmail.com <inkago1@gmail.com@d1cde06e-e40a-6fc9-11d7-2fea08263baf>
2012-06-02 19:13:15 +0000

    before everything deletes itself...

--- trunk/AzLaboratory/src/java/bgu/dcr/az/lab/CpuBean.java
+++ trunk/AzLaboratory/src/java/bgu/dcr/az/lab/CpuBean.java
@@ -8,11 +8,13 @@ import bgu.dcr.az.lab.data.Cpu;
 import bgu.dcr.az.lab.data.Experiments;
 import bgu.dcr.az.lab.data.Users;
 import bgu.dcr.az.lab.db.DBManager;
-import java.io.Serializable;
 import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import org.primefaces.event.DragDropEvent;
 
 /**
@@ -20,50 +22,72 @@ import org.primefaces.event.DragDropEvent;
  * @author Inna
  */
 @ManagedBean(eager = true)
-public class CpuBean{
-    
+@SessionScoped
+public class CpuBean {
+
     Experiments selectedExperiment;
-    HashMap<Integer, List<Experiments>> droppedExperiments = new HashMap<Integer, List<Experiments>>();
+    List<CpuEntry> cpuMap;
+    List<Experiments> experiments;
 
-    public List<Experiments> getExperiments() throws SQLException {
-        List<Experiments> ans = DBManager.UNIT.getUserExperiments((Users) SessionManager.getFromSessionMap(SessionManager.CURRENT_USER));
-        System.out.println("GOT Experiments " + ans);
-        return ans;
-    }
-    
-    public List<Cpu> getCpus() throws SQLException {
-        List<Cpu> ans = DBManager.UNIT.getUserCpus((Users) SessionManager.getFromSessionMap(SessionManager.CURRENT_USER));
-        System.out.println("GOT CPUS " + ans);
-        return ans;
-    }
-    
-    
-
-    public void deleteExperiment() {
-        System.out.println("DELETED ");
+    public CpuBean() {
+        Users user = (Users) SessionManager.getFromSessionMap(SessionManager.CURRENT_USER);
+        List<Cpu> cpus = DBManager.UNIT.getUserCpus(user);
+        populateCpuMap(cpus);
+        System.out.println("User is " + user.getName() + "CPUS are here " + cpus);
+//        droppedExperiments = new Experiments[cpus.size()];
+        experiments = DBManager.UNIT.getUserExperiments((Users) SessionManager.getFromSessionMap(SessionManager.CURRENT_USER));
     }
 
-    public void deleteAlgorithm() {
+    public List<CpuEntry> getCpuMap() {
+        return cpuMap;
+    }
+
+    public List<Experiments> getExperiments() {
+        return experiments;
     }
 
     public Experiments getSelectedExperiment() {
-        System.out.println("GOT selected experiment " + selectedExperiment.getId());
+//        System.out.println("GOT selected experiment " + selectedExperiment.getId());
         return selectedExperiment;
     }
 
     public void setSelectedExperiment(Experiments selectedExperiment) {
-        System.out.println("SET selected experiment " + selectedExperiment.getId());
+//        System.out.println("SET selected experiment " + selectedExperiment.getId());
         this.selectedExperiment = selectedExperiment;
     }
-    
-    public List<Experiments> getDroppedExperiments(int id){
-        System.out.println("GOT dropped experiments " + droppedExperiments.get(id));
-        return droppedExperiments.get(id);
+
+    public void onExperimentDrop(DragDropEvent ddEvent) {
+        Experiments exp = ((Experiments) ddEvent.getData());
+        int id = getId(ddEvent.getDropId());
+        cpuMap.get(getEntryByIndex(id)).setExp(exp);
+        System.out.println("id is" + id);
+
+//        System.out.println("SET dropped experiments " + droppedExperiments.get(1) + "to " + exp);
+
+//        droppedExperiments[id] = exp;
+//        System.out.println("SET dropped experiments " );
     }
-    
-    public void onExperimentDrop(DragDropEvent ddEvent) {  
-        Experiments exp = ((Experiments) ddEvent.getData());  
-        System.out.println("SET dropped experiments " + droppedExperiments.get(1) + "to " + exp);
-        (droppedExperiments.get(1)).add(exp);
-    } 
+
+    private int getId(String dropId) {
+        return Integer.valueOf(dropId.split(":x")[1]);
+    }
+
+    private void populateCpuMap(List<Cpu> cpus) {
+        cpuMap = new LinkedList<CpuEntry>();
+        for (int i = 0; i < cpus.size(); i++) {
+            cpuMap.add(new CpuEntry(i, cpus.get(i)));
+        }
+        System.out.println("CPU-MAP is " + cpuMap.toString());
+    }
+
+    private int getEntryByIndex(int index) {
+
+        for (int i = 0; i < cpuMap.size(); i++) {
+            CpuEntry cpu = cpuMap.get(i);
+            if (cpu.getIndex() == index) {
+                return i;
+            }
+        }
+        return 0;
+    }
 }

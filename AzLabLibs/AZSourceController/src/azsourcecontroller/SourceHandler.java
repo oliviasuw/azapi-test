78405f8dc929b30b878c611bb0c34d635f8d37c7
dima3685@gmail.com <dima3685@gmail.com@d1cde06e-e40a-6fc9-11d7-2fea08263baf>
2012-07-30 14:46:04 +0000

    source controller

---
+++ AzLabLibs/AZSourceController/src/azsourcecontroller/SourceHandler.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package azsourcecontroller;
+
+import java.io.File;
+
+/**
+ *
+ * @author kdima85
+ */
+public class SourceHandler {
+    
+    
+    public void init(){
+        
+    }
+    
+    /**
+     * will save the un-zipped file to generated temp folder
+     * will parse the un-zipped package
+     * return analyzed Object of all the enteties in the package
+     * @param zipped 
+     */
+    public void parsePackage(long userId, File zippedPackage){
+            
+    }
+    
+    public void saveTempToDB(long userId, File tmpDir){
+        
+    }
+}

548b2f94415b7c3352f552a35ae6de52268fea96
benny.lutati@gmail.com <benny.lutati@gmail.com@d1cde06e-e40a-6fc9-11d7-2fea08263baf>
2012-08-02 22:04:21 +0000

    visualizations works.

--- AZ Eclipse Plugin/Agent0Dev/src/agt0/dev/SharedDataUnit.java
+++ AZ Eclipse Plugin/Agent0Dev/src/agt0/dev/SharedDataUnit.java
@@ -1,9 +1,12 @@
 package agt0.dev;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
 
 import agt0.dev.util.EclipseUtils;
+import agt0.dev.util.FileUtils;
 
 /**
  * this called the shared unit cause it is shared between projects
@@ -13,13 +16,18 @@ import agt0.dev.util.EclipseUtils;
 public enum SharedDataUnit {
 	UNIT;
 	
-	private static final String LIBRARY_PATH = EclipseUtils
-			.getWorkspaceDirectory().getAbsolutePath() + "/.az/lib";
+	private static final String AZ_WORKSPACE_PATH = EclipseUtils
+			.getWorkspaceDirectory().getAbsolutePath() + "/.az";
 	
-	private static final String DOC_PATH = EclipseUtils
-			.getWorkspaceDirectory().getAbsolutePath() + "/.az/doc";
+	private static final String LIBRARY_PATH = AZ_WORKSPACE_PATH + "/lib";
+	
+	private static final String DOC_PATH = AZ_WORKSPACE_PATH + "/doc";
 	
 	public static final String JAVADOC_LOCATION_IN_ARCHIVE = "/";
+	
+	public static final String JFX_LOCATION_STORE_FILE_NAME = "jfx.location";
+	
+	public static final String JFX_LOCATION_STORE_FILE_PATH = AZ_WORKSPACE_PATH + "/jfx.location";
 	
 	/**
 	 * @return list of all the jars that stored in the lib folder or empty list if no such folder
@@ -52,4 +60,38 @@ public enum SharedDataUnit {
 		return new File(DOC_PATH + "/javadoc.jar" );
 	}
 	
+	public File findJavaFxRuntime(){
+		File jfxLocationFile = new File(JFX_LOCATION_STORE_FILE_PATH);
+		if (jfxLocationFile.exists()){
+			try {
+				return new File(FileUtils.unPersistText(jfxLocationFile));
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		if (System.getProperty("os.name").startsWith("Windows")){
+			File root = new File("/Program Files/Oracle/");
+			for (File f : root.listFiles()){
+				if (f.getName().startsWith("JavaFX") && f.getName().endsWith("Runtime")){
+					return f;
+				}
+			}
+		}
+		
+		return null;
+	}
+	
+	public void storeJavaFxRuntimeLocation(File where){
+		try {
+			FileUtils.persistText(new File(AZ_WORKSPACE_PATH), JFX_LOCATION_STORE_FILE_NAME, where.getAbsolutePath());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
--- AZ Eclipse Plugin/Agent0Dev/src/agt0/dev/ui/WhereIsJFXDialog.java
+++ AZ Eclipse Plugin/Agent0Dev/src/agt0/dev/ui/WhereIsJFXDialog.java
@@ -1,31 +1,32 @@
 package agt0.dev.ui;
 
-import java.awt.Desktop;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.io.File;
 
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.IDialogConstants;
-import org.eclipse.swt.graphics.Point;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.SWT;
-import org.eclipse.wb.swt.SWTResourceManager;
-import org.eclipse.wb.swt.ResourceManager;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Link;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.wb.swt.ResourceManager;
+import org.eclipse.wb.swt.SWTResourceManager;
 
-import agt0.dev.FrameworkUpdateUnit;
-import agt0.dev.Global;
+import swing2swt.layout.FlowLayout;
+import agt0.dev.SharedDataUnit;
 
 public class WhereIsJFXDialog extends Dialog {
-
+	private Text jfxlocText;
+	private String jfxLocation;
+	
 	/**
 	 * Create the dialog.
 	 * @param parentShell
@@ -43,17 +44,35 @@ public class WhereIsJFXDialog extends Dialog {
 		Composite container = (Composite) super.createDialogArea(parent);
 		container.setLayout(new GridLayout(2, false));
 		
-		Label label = new Label(container, SWT.NONE);
-		label.setImage(ResourceManager.getPluginImage("Agent0Dev", "icons/dialogs/found-bug.png"));
+		Composite composite = new Composite(container, SWT.NONE);
+		composite.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 2, 1));
+		composite.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
-		Label lblAgentZeroFramework = new Label(container, SWT.NONE);
-		lblAgentZeroFramework.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1));
+		Label label = new Label(composite, SWT.NONE);
+		label.setImage(ResourceManager.getPluginImage("Agent0Dev", "icons/dialogs/where.png"));
+		
+		Label lblAgentZeroFramework = new Label(composite, SWT.NONE);
 		lblAgentZeroFramework.setFont(SWTResourceManager.getFont("Segoe UI", 14, SWT.BOLD));
 		lblAgentZeroFramework.setText("JavaFx Runtime Nedded.");
 		
 		Label lblNewLabel = new Label(container, SWT.NONE);
 		lblNewLabel.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 2, 1));
-		lblNewLabel.setText("\r\nPlease submit it in Agent Zero bug tracking system,\r\nwhen submitting bugs dont forgot to write enough data so that \r\nthe developer will be able to reproduce the bug \r\nif Agent Zero crushed - copy the exception and attach the project with the test.xml file\r\nand the algorithm that cause the failure\r\n\r\ndo you want to open the bug tracking system?");
+		lblNewLabel.setText("\r\nAgentZero Visualization Viewer requires JavaFx runtime.\r\nAgentZero could not locate the required runtime in your system.\r\nyou can download JavaFx runtime by following the link bellow.");
+		
+		Link link = new Link(container, SWT.NONE);
+		link.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 2, 1));
+		link.setText("<a href=\"http://www.oracle.com/technetwork/java/javafx/downloads/index.html\">JavaFx Runtime</a>");
+		
+		Label lblIfYouAlready = new Label(container, SWT.NONE);
+		lblIfYouAlready.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 2, 1));
+		lblIfYouAlready.setText("\r\nIf you already install JavaFx runtime but still seeing this message\r\nplease supply the location to the runtime installation \r\n(default installation location on windows machine is: C:\\Program Files\\Oracle\\JavaFX <VERSION> Runtime)");
+		
+		Label lblJavafxRuntimeLocation = new Label(container, SWT.NONE);
+		lblJavafxRuntimeLocation.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		lblJavafxRuntimeLocation.setText("JavaFx runtime location");
+		
+		jfxlocText = new Text(container, SWT.BORDER);
+		jfxlocText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 
 		return container;
 	}
@@ -64,23 +83,18 @@ public class WhereIsJFXDialog extends Dialog {
 	 */
 	@Override
 	protected void createButtonsForButtonBar(Composite parent) {
-		Button btnUpdate = createButton(parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL,
+		Button btnUpdate = createButton(parent, IDialogConstants.CLIENT_ID + 1, "Execute",
 				true);
 		btnUpdate.addSelectionListener(new SelectionAdapter() {
 			@Override
-			public void widgetSelected(SelectionEvent e) {
-				try {
-					Desktop.getDesktop().browse(new URI(Global.BUG_TRACKING_SYSTEM_URL.string()));
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				} catch (URISyntaxException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
+			public void widgetSelected(SelectionEvent e) { 
+				if (!jfxlocText.getText().isEmpty()){
+					SharedDataUnit.UNIT.storeJavaFxRuntimeLocation(new File(jfxlocText.getText()));
 				}
+				
+				close();
 			}
 		});
-		btnUpdate.setText("Open");
 		Button button_1 = createButton(parent, IDialogConstants.CANCEL_ID,
 				IDialogConstants.CANCEL_LABEL, false);
 		button_1.addSelectionListener(new SelectionAdapter() {
@@ -96,7 +110,9 @@ public class WhereIsJFXDialog extends Dialog {
 	 */
 	@Override
 	protected Point getInitialSize() {
-		return new Point(561, 284);
+		return new Point(648, 329);
 	}
 
+	
+	
 }

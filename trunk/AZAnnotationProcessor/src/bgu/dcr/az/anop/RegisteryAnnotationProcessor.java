5e32cd54594f318f0459f27e9d5f3d2fce06d6d0
benny.lutati@gmail.com <benny.lutati@gmail.com@d1cde06e-e40a-6fc9-11d7-2fea08263baf>
2014-01-23 13:31:44 +0000

    some little fixing...

--- trunk/AZAnnotationProcessor/src/bgu/dcr/az/anop/RegisteryAnnotationProcessor.java
+++ trunk/AZAnnotationProcessor/src/bgu/dcr/az/anop/RegisteryAnnotationProcessor.java
@@ -155,12 +155,12 @@ public class RegisteryAnnotationProcessor extends AbstractProcessor {
                     info.setter = setterName;
                 }
 
-                note("adding " + p.getKey() + " with doc: " + processingEnv.getElementUtils().getDocComment(p.getValue()));
-                JavaDocInfo jd = JavaDocParser.parse(processingEnv.getElementUtils().getDocComment(p.getValue()));
-                note(jd.description());
-                for (String t : jd.tags()) {
-                    note(t + ":\n" + jd.tag(t));
-                }
+//                note("adding " + p.getKey() + " with doc: " + processingEnv.getElementUtils().getDocComment(p.getValue()));
+//                JavaDocInfo jd = JavaDocParser.parse(processingEnv.getElementUtils().getDocComment(p.getValue()));
+//                note(jd.description());
+//                for (String t : jd.tags()) {
+//                    note(t + ":\n" + jd.tag(t));
+//                }
                 
                 properties.add(info);
             }

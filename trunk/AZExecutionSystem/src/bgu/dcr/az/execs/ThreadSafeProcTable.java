71847e54a0c028d901c078174e08ba79b869ad86
benny.lutati@gmail.com <benny.lutati@gmail.com@d1cde06e-e40a-6fc9-11d7-2fea08263baf>
2014-01-26 17:16:00 +0000

    experiment can now contain several tests (+ another idle detection bug fixed)

--- trunk/AZExecutionSystem/src/bgu/dcr/az/execs/ThreadSafeProcTable.java
+++ trunk/AZExecutionSystem/src/bgu/dcr/az/execs/ThreadSafeProcTable.java
@@ -14,6 +14,7 @@ import java.util.LinkedList;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
@@ -24,7 +25,7 @@ import java.util.logging.Logger;
  * @author bennyl
  */
 public class ThreadSafeProcTable implements ProcTable {
-
+    
     private final Map<Integer, ProcessInfo> processInfos = new ConcurrentHashMap<>();
     private final LinkedBlockingQueue<ProcessInfo> pendingProcesses = new LinkedBlockingQueue<>();
     private final AtomicInteger blockingProcesses = new AtomicInteger(0);
@@ -33,20 +34,21 @@ public class ThreadSafeProcTable implements ProcTable {
     //signaling cores in blocked in pending queue that there is an idle
     private final ProcessInfo currentIdleSignal = new ProcessInfo(null);
     private final AtomicInteger estimatedWaitingCores = new AtomicInteger(0);
-
+    
     EventListeners<ProcTableListener> listeners = EventListeners.create(ProcTableListener.class);
-
+    
     @Override
     public EventListeners<ProcTableListener> listeners() {
         return listeners;
     }
-
+    
     @Override
     public Proc acquire() throws InterruptedException {
         while (true) {
             estimatedWaitingCores.incrementAndGet();
-
+            
             if (isInIdleState() || isEmpty()) {
+                
                 pendingProcesses.add(currentIdleSignal); //to release the next agent
                 return null;
             }
@@ -57,7 +59,7 @@ public class ThreadSafeProcTable implements ProcTable {
             if (isIdleSignal(next)) {
                 continue;
             }
-
+            
             boolean mine = next.acquired.compareAndSet(false, true);
             if (mine && next.process.state() != ProcState.TERMINATED) {
 //                System.out.println("Taken: " + next.process.pid());
@@ -68,53 +70,81 @@ public class ThreadSafeProcTable implements ProcTable {
             }
         }
     }
-
+    
     private boolean isIdleSignal(ProcessInfo next) {
         return next == currentIdleSignal;
     }
-
+    
     private ProcessInfo retreiveProcessInfo(int pid) {
         return processInfos.get(pid);
     }
-
+    
     @Override
     public void release(int pid) {
         ProcessInfo proc = retreiveProcessInfo(pid);
         release(proc);
     }
-
+    
     private void wake(ProcessInfo procInfo) {
-        procInfo.signaled.set(true);
-
-        if (procInfo.inQueue.compareAndSet(false, true)) {
-            final int blocking = blockingProcesses.decrementAndGet();
-            if (blocking < 0) {
-                throw new RuntimeException("BAD IMPLEMENTATION OF PROC-TABLE.");
+        if (procInfo.signaled.compareAndSet(false, true)) {
+            try {
+                procInfo.signalHandlerLock.acquire();
+                
+                try {
+                    if (procInfo.inQueue.compareAndSet(false, true)) {
+                        final int blocking = blockingProcesses.decrementAndGet();
+                        if (blocking < 0) {
+                            throw new RuntimeException("BAD IMPLEMENTATION OF PROC-TABLE.");
+                        }
+                        pendingProcesses.add(procInfo);
+                    } else {
+                        procInfo.signaled.set(true);
+                    }
+                } finally {
+                    procInfo.signalHandlerLock.release();
+                }
+                
+            } catch (InterruptedException ex) {
+                Logger.getLogger(ThreadSafeProcTable.class.getName()).log(Level.SEVERE, null, ex);
+                Thread.currentThread().interrupt();
             }
-
-            pendingProcesses.add(procInfo);
         }
+        
     }
-
+    
     private void release(ProcessInfo proc) {
         ProcState state = proc.process.state();
-
+        
         proc.acquired.set(false);
-
+        
         switch (state) {
             case INITIALIZING:
             case RUNNING:
                 pendingProcesses.add(proc);
                 break;
             case BLOCKING:
-
-                if (proc.signaled.compareAndSet(true, false)) {
-                    pendingProcesses.add(proc);
-                } else {
-                    blockingProcesses.incrementAndGet();
-                    proc.inQueue.set(false);
+                
+                try {
+                    proc.signalHandlerLock.acquire();
+                    
+                    try {
+                        
+                        if (proc.signaled.compareAndSet(true, false)) { //double check to eliminate a race condition..
+                            pendingProcesses.add(proc);
+                        } else {
+                            blockingProcesses.incrementAndGet();
+                            proc.inQueue.set(false);
+                        }
+                        
+                    } finally {
+                        proc.signalHandlerLock.release();
+                    }
+                    
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(ThreadSafeProcTable.class.getName()).log(Level.SEVERE, null, ex);
+                    Thread.currentThread().interrupt();
                 }
-
+                
                 break;
             case TERMINATED:
 //                System.out.println("process " + pid + " terminated");
@@ -124,11 +154,11 @@ public class ThreadSafeProcTable implements ProcTable {
                 break;
             default:
                 throw new AssertionError(state.name());
-
+            
         }
-
+        
     }
-
+    
     @Override
     public boolean wake(int pid) {
         ProcessInfo procInfo = retreiveProcessInfo(pid);
@@ -136,52 +166,52 @@ public class ThreadSafeProcTable implements ProcTable {
             wake(procInfo);
             return true;
         }
-
+        
         return false;
     }
-
+    
     @Override
     public void add(Proc p) {
         ProcessInfo pinfo = new ProcessInfo(p);
-
+        
         processInfos.put(p.pid(), pinfo);
-
+        
         pinfo.inQueue.set(true);
         pinfo.acquired.set(false);
         totalNumberOfProcesses.incrementAndGet();
-
+        
         pendingProcesses.add(pinfo);
         listeners.fire().onProcessAdded(this, p.pid());
     }
-
+    
     @Override
     public boolean isInIdleState() {
 
         //since additions of processes are assumed to be only performed by a process once the execution started
         //it is safe to say that if in any point in time the number of blocking processes reach the number of processes
         //(by this order) then we reached to an idle.
-        return totalNumberOfProcesses.get() == blockingProcesses.get() && !processInfos.isEmpty();
+        return totalNumberOfProcesses.get() == blockingProcesses.get();
     }
-
+    
     @Override
     public boolean isEmpty() {
         return totalNumberOfProcesses.get() == 0;
     }
-
+    
     @Override
     public int nextProcessId() {
         return nextProceId.getAndIncrement();
     }
-
+    
     @Override
     public void startIdleDetectionResolving(IdleDetectionResolver resolver) {
 //        blockingProcesses.set(0);
         int numWake = totalNumberOfProcesses.get();
-
+        
         for (ProcessInfo p : processInfos.values()) {
             wake(p);
         }
-
+        
         try {
             for (int i = 0; i < numWake; i++) {
                 Proc p = acquire();
@@ -193,28 +223,29 @@ public class ThreadSafeProcTable implements ProcTable {
             Thread.currentThread().interrupt();
         }
     }
-
+    
     @Override
     public Collection<Integer> allProcessIds() {
         LinkedList<Integer> all = new LinkedList<>();
         for (ProcessInfo a : processInfos.values()) {
             all.add(a.process.pid());
         }
-
+        
         return all;
     }
-
+    
     private static class ProcessInfo {
-
+        
         Proc process;
         AtomicBoolean acquired = new AtomicBoolean(false);
         AtomicBoolean inQueue = new AtomicBoolean(false);
         AtomicBoolean signaled = new AtomicBoolean(false);
-
+        Semaphore signalHandlerLock = new Semaphore(1);
+        
         public ProcessInfo(Proc process) {
             this.process = process;
         }
-
+        
     }
-
+    
 }

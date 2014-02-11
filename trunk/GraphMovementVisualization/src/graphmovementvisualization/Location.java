cd41ef8aa8a4023214bbda9e2998987370b49e65
shlomi.cho@gmail.com <shlomi.cho@gmail.com@d1cde06e-e40a-6fc9-11d7-2fea08263baf>
2014-02-11 20:25:01 +0000

    just trying to put a 100 moving images on a canvas, clearing it and re-drawing on each iteration.

---
+++ trunk/GraphMovementVisualization/src/graphmovementvisualization/Location.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package graphmovementvisualization;
+
+import javafx.beans.value.WritableValue;
+
+/**
+ *
+ * @author Shl
+ */
+public class Location {
+    
+    private double x;
+    private double y;
+
+    public Location() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public Location(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    
+    
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 71 * hash + (int) (Double.doubleToLongBits(this.x) ^ (Double.doubleToLongBits(this.x) >>> 32));
+        hash = 71 * hash + (int) (Double.doubleToLongBits(this.y) ^ (Double.doubleToLongBits(this.y) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Location other = (Location) obj;
+        if (Double.doubleToLongBits(this.x) != Double.doubleToLongBits(other.x)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.y) != Double.doubleToLongBits(other.y)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+    
+}

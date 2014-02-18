2744187298f9a9f0eea9fab39f7c83e804115794
shlomi.cho@gmail.com <shlomi.cho@gmail.com@d1cde06e-e40a-6fc9-11d7-2fea08263baf>
2014-02-18 14:18:25 +0000

    added scaling so that tomorrow i can work only on afc.

--- trunk/GraphMovementVisualization/src/graphicx/sprite/Sprite.java
+++ trunk/GraphMovementVisualization/src/graphicx/sprite/Sprite.java
@@ -22,6 +22,7 @@ public class Sprite {
     protected final long index;
     protected final ObjectAnimator animator;
     protected double angle = 0;
+    private double scale = 1;
 
     public Sprite(long index) {
         InputStream resourceAsStream = getClass().getResourceAsStream("car.jpg");
@@ -41,14 +42,15 @@ public class Sprite {
         double tx = canvas.getTranslateX();
         double ty = canvas.getTranslateY();
 
-        double drawX = location.getX() - tx;
-        double drawY = location.getY() - ty;
+        //notice that the scale here always has to be equal to the canvas scale!!!
+        double drawX = (location.getX() - tx)*scale;
+        double drawY = (location.getY() - ty)*scale;
 
         gc.save(); // saves the current state on stack, including the current transform
         gc.translate(drawX, drawY);
         gc.rotate(angle);
-        gc.translate(-image.getWidth() / 2.0, -image.getHeight() / 2.0);
-        gc.drawImage(image, 0, 0);
+        gc.translate(-(image.getWidth()*scale) / 2.0, -(image.getHeight()*scale) / 2.0);
+        gc.drawImage(image, 0, 0, image.getWidth()*scale, image.getHeight()*scale);
         gc.restore(); // back to original state (before rotation)
 
 //                    gcAction.drawImage(sprite.getImage(), location.getX().doubleValue() - tx, location.getY().doubleValue() - ty);
@@ -65,6 +67,16 @@ public class Sprite {
     public void setRotation(double rotation) {
         this.angle = rotation;
     }
+    
+    public void setScale(double scale) {
+        this.scale = scale;
+    }
+
+    public double getScale() {
+        return scale;
+    }
+    
+    
 
     public ObjectAnimator getAnimator() {
         return animator;
